95b1a9d06cff0cc02800198282e84406630083d2
Dimitrios Amoutzidis <e-supporting@hotmail.com>
2015-12-04 11:41:44 +0200

    third commit

--- app/src/main/java/com/amou/traditionalsongs/activities/SpashActivity.java
+++ app/src/main/java/com/amou/traditionalsongs/activities/SpashActivity.java
@@ -14,5 +14,6 @@ public class SpashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //adsddsa
     }
 }
